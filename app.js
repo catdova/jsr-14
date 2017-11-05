190d537da88069103dca3eaa95fc01187340decd
Kim Cordova <cordova.k529@gmail.com>
2017-11-05 18:09:49 -0500

    save

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function FarmAnimal(name, sound, img) {
 	this.name = name;
 	this.sound = sound;
 	this.img = 'animals/' + name + '.png';
-	this.talk = "the " + name + "says " + sound;
+	this.talk = "the " + name + " says " + sound;
 }
 
 function LiveStock(name, sound, img){
@@ -16,22 +16,39 @@ function Bird(name, sound, img) {
 	this.eggs = true;
 }
 
+//create animals//
 var goose = new Bird('goose', 'honk');
 var cow = new LiveStock('cow', 'moo');
 var horse = new FarmAnimal('horse', 'neigh');
 var pig = new LiveStock('pig', 'oink oink');
 var turkey = new Bird('turkey', 'gobble gobble');
 
-console.log(goose.img)
+//create images from objects//
+var gooseImage = "<img src='" + goose.img + "' class='animal' id='gooseImage'>";
+var cowImage = "<img src='" + cow.img + "' class='animal' id='cowImage' >";
+var horseImage = "<img src='" + horse.img + "' class='animal' id='horseImage' >";
+var pigImage = "<img src='" + pig.img + "' class= animal id='pigImage'>";
+var turkeyImage = "<img src='" + turkey.img + "' class= animal id='turkeyImage'>";
 
-var gooseImage = "<img src='" + goose.img + "' class= animal >";
-var cowImage = "<img src='" + cow.img + "' class= animal >";
-var horseImage = "<img src='" + horse.img + "' class= animal >";
-var pigImage = "<img src='" + pig.img + "' class= animal >";
-var turkeyImage = "<img src='" + turkey.img + "' class= animal >";
+//add animal images to containers//
+$('#livestock-container').append(cowImage, pigImage);
+$('#bird-container').append(gooseImage, turkeyImage);
+$('#horse-container').append(horseImage);
 
 
-$('animal-container').append(gooseImage)
+//Aniaml sounds//
+$('#gooseImage').click(function(){
+	alert(goose.sound)
+	console.log(goose.talk);
+});
+
+$('#horseImage').click(function(){
+	alert(horse.sound)
+	console.log(horse.talk);
+});
+
+
+
 
 
 
